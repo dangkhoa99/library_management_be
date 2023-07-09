2cb945af5690545150241064f63822c515260138
Khoa Nguyen <nguyenminhdangkhoa9@gmail.com>
2023-07-09 21:10:32 +0700

    add route user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const authRouter = require('./auth.routes')
+const userRouter = require('./user.routes')
 const categoryRouter = require('./category.routes')
 
 function routes(app) {
   app.use('/api/auth', authRouter)
+  app.use('/api/users', userRouter)
   app.use('/api/categories', categoryRouter)
 }
 
---
+++ routes/user.routes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../controllers/user.controller')
+const verifyToken = require('../middleware/verifyToken.middleware')
+
+router.use(verifyToken)
+
+router.get('/', User.list)
+router.get('/authors', User.listAuthor)
+router.get('/customers', User.listCustomer)
+router.get('/librarians', User.listLibrarian)
+router.get('/:id', User.show)
+
+router.post('/', User.create)
+router.post('/authors', User.createAuthor)
+router.post('/customers', User.createCustomer)
+router.post('/librarians', User.createLibrarian)
+router.post('/changePassword', User.changePassword)
+
+router.patch('/:id', User.edit)
+
+router.delete('/:id', User.delete)
+
+module.exports = router
