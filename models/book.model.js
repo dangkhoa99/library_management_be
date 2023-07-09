777d4b74ff5aa692bf458b644e0dca1783445bd3
Khoa Nguyen <nguyenminhdangkhoa9@gmail.com>
2023-07-09 21:58:40 +0700

    fix ref in book model

--- models/book.model.js
+++ models/book.model.js
@@ -6,7 +6,7 @@ const bookSchema = new Schema(
     name: { type: String, required: true },
     description: { type: String },
     category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
-    author: { type: Schema.Types.ObjectId, ref: 'Author', required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     quantity: { type: Number, default: 1 },
     publisher: { type: String },
     publishDate: { type: Date },
